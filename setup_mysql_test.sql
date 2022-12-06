fdb40727f3a2528cef47131ffdf9acb57c90a2f3
kendimuthomi <kendijackline42@gmail.com>
2022-12-06 08:48:02 +0000

    HBNB CONSOLE AND MYSQL

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,16 +4,9 @@
 --   Grants all privileges for hbnb_test on hbnb_test_db.
 --   Grants SELECT privilege for hbnb_test on performance_schema.
 
-CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 USE `hbnb_test_db`;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-	ON `hbnb_test_db`.*
-	TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-	ON `performance_schema`.*
-	TO 'hbnb_test'@'localhost'
-	IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
