e1dc633bd5da38f912bd17a56f1e49c12d8b46dd
kendimuthomi <kendijackline42@gmail.com>
2022-12-06 12:47:51 +0000

    Type of storage not environment

--- models/base_model.py
+++ models/base_model.py
@@ -54,4 +54,4 @@ class BaseModel:
     def delete(self):
         """delete the current instance from the storage"""
         from models import storage
-        storage.delete()
+        storage.delete(self)
