b15f96ec28195032cbea278c0139d3627892cb02
kendimuthomi <kendijackline42@gmail.com>
2022-12-06 11:39:32 +0000

    git add .!

---
+++ models/user_LOCAL_24972.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module defines a class User"""
+from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+
+
+class User(BaseModel):
+    """This class defines a user by various attributes"""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+    places = relationship("Place", backref="user', cascade="delete")
