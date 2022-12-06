650b07ebda0c8db52f03183a3d9e642fca98706d
brianmugunga <104896959+brianmugunga@users.noreply.github.com>
2022-12-06 18:02:42 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-import models
+"""This is the user class"""
 from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey, Integer, Float
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+class User(BaseModel, Base):
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship('Place', backref='user', cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
-
-    def __init__(self, *args, **kwargs):
-        """
-        inherit from base  and Basemodel init
-        """
-        super().__init__(*args, **kwargs)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
