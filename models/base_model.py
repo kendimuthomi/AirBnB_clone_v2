a4b67ec2b03a4da9f3cfc1a50659720b6229ad93
kendimuthomi <kendijackline42@gmail.com>
2022-12-06 08:36:55 +0000

    HBNB CONSOLE AND MYSQL

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        format_str = '%Y-%m-%dT%H:%M:%S.%f'
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -15,12 +16,14 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, format_str)
+                else:
+                    if key != "__class__":
+                        self.__dict__[key] = value
+            if 'id' not in self.__dict__.keys():
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
