841059e08a96b376e28567f026204e69147d9bc9
kendimuthomi <kendijackline42@gmail.com>
2022-12-07 03:47:42 +0000

    Updating User

--- models/user.py
+++ models/user.py
@@ -8,12 +8,15 @@ from sqlalchemy.orm import relationship
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
-    email = Column('email', String(128), nullable=False)
-    password = Column('password', String(128), nullable=False)
-    first_name = Column('first_name', String(128), nullable=True, default="NULL")
-    last_name = Column('last_name', String(128), nullable=True, default="NULL")
-    # backref may need to be back_populates?Below line commented out bc console
-    # would not run with it in. This line was implemented in Task 8
-    places = relationship("Place", cascade="delete", backref="user")
-    # Below line is commented out for caution and was added in Task 9
-    reviews = relationship("Review", cascade="delete", backref="user")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column('email', String(128), nullable=False)
+        password = Column('password', String(128), nullable=False)
+        first_name = Column('first_name', String(128), nullable=True, default="NULL")
+        last_name = Column('last_name', String(128), nullable=True, default="NULL")
+        places = relationship("Place", cascade="delete", backref="user")
+        reviews = relationship("Review", cascade="delete", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
