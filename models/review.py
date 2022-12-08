0076313168376119d1bcde0c70d73ba22b278880
kendimuthomi <kendijackline42@gmail.com>
2022-12-08 10:44:36 +0000

    How could I forget pycodestyle?

--- models/review.py
+++ models/review.py
@@ -7,7 +7,7 @@ from os import getenv
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    __tablename__ = "reviews" 
+    __tablename__ = "reviews"
     if getenv("HBNB_TYPE_STORAGE") == "db":
         text = Column(String(1024), nullable=False)
         place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
--- models/user.py
+++ models/user.py
@@ -12,10 +12,15 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         email = Column('email', String(128), nullable=False)
         password = Column('password', String(128), nullable=False)
-        first_name = Column('first_name', String(128), nullable=True, default="NULL")
-        last_name = Column('last_name', String(128), nullable=True, default="NULL")
-        places = relationship('Place', backref='user', cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user', cascade='all, delete, delete-orphan')
+        first_name = Column('first_name',
+                            String(128), nullable=True,
+                            default="NULL")
+        last_name = Column('last_name', String(128),
+                           nullable=True, default="NULL")
+        places = relationship('Place', backref='user',
+                              cascade='all, delete, delete-orphan')
+        reviews = relationship('Review', backref='user',
+                               cascade='all, delete, delete-orphan')
     else:
         email = ""
         password = ""
