93165ba48f0ccd72c360bef42c18e50383aca9f2
kendimuthomi <kendijackline42@gmail.com>
2023-01-16 10:43:38 +0000

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,11 +10,13 @@ def hello():
     """print a message when route / is used"""
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """print a message when /hbnb is used"""
     return "HBNB"
 
+
 if __name__ == '__main__':
     """Main function"""
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
