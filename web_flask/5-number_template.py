92fabaabe573cd503197527f45d542827bf67767
kendimuthomi <kendijackline42@gmail.com>
2023-01-16 12:07:44 +0000

    templates

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """print an html template"""
-    return render_template('templates/5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
