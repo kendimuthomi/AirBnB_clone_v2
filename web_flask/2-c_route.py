e6f0c8d0a63b701c13a6075de1e04c49c40f031c
kendimuthomi <kendijackline42@gmail.com>
2023-01-16 10:55:00 +0000

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Using Flask"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """print a message when route / is used"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """print a message when /hbnb is used"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun():
+    """print C plus text"""
+    return "C" + text.replace("_", " ")
+
+
+if __name__ == '__main__':
+    """Main function"""
+    app.run(host='0.0.0.0', port=5000)
