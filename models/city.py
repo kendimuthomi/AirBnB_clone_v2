b5123dbf8ceb6af49ccd5ed35d1920061e068de5
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-01-18 20:23:47 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
-"""This is the city class"""
+""" City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.place import Place
-from sqlalchemy import Column, Integer, String, ForeignKey
-from os import getenv
+from models import storage_type
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
-    """
-    __tablename__ = "cities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
+    """ The city class, contains state ID and name """
+    __tablename__ = 'cities'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship('Place',
-                              backref='cities',
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', backref='cities',
                               cascade='all, delete, delete-orphan')
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for i, j in kwargs.items():
-                setattr(self, i, j)
     else:
         name = ''
         state_id = ''
