63ff213be07579534796041a524c79a3b6a1ce5f
kendimuthomi <96398605+kendimuthomi@users.noreply.github.com>
2023-01-18 20:28:20 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from models import storage_type
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        email = Column('email', String(128), nullable=False)
-        password = Column('password', String(128), nullable=False)
-        first_name = Column('first_name',
-                            String(128), nullable=True,
-                            default="NULL")
-        last_name = Column('last_name', String(128),
-                           nullable=True, default="NULL")
+    __tablename__ = 'users'
+    if storage_type == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
         places = relationship('Place', backref='user',
                               cascade='all, delete, delete-orphan')
         reviews = relationship('Review', backref='user',
